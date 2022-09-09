07dbaa398f055f8402c068f9fc238bc5b82fb33b
InnaPavan <innapavan@gmail.com>
2022-09-09 13:28:27 +0100

    nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -6,7 +6,7 @@ NEXUS_URL=http://13.42.12.118:8081
 MAVEN_REPO=maven-releases 
 GROUP_ID=org.springframework.samples
 ARTIFACT_ID=spring-petclinic
-VERSION=2.6.3
+VERSION=2.6.4
 FILE_EXTENSION=jar
 
 download_url=$(curl -L -u admin:Summer@123 -X GET "${NEXUS_URL}/service/rest/v1/search/assets?repository=${MAVEN_REPO}&maven.groupId=${GROUP_ID}&maven.artifactId=${ARTIFACT_ID}&maven.baseVersion=${VERSION}&maven.extension=${FILE_EXTENSION}" -H  "accept: application/json" | jq -rc '.items | .[].downloadUrl') 
